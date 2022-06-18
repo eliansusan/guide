04b894c0cdb59a82b66bbd2b064e27c5593a530f
eliansusan <107761548+eliansusan@users.noreply.github.com>
2022-06-18 16:42:36 +0100

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,54 @@
+package com.guideayoub.jsonayoub;
+
+import android.app.Activity;
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+public class Utils {
+    //----------privacy url
+    public static String privacyUrl;
+    //--------------
+    public static  String ONESIGNAL_APP_ID = "########-####-####-####-############";
+
+    //chose your mood(offline or online)
+    public static String Mood="online";
+    //----------Admob Ad----------------
+    public static String Interstitial_Admob="ca-app-pub-3940256099942544/1033173712";
+    public static String Native_Admob="ca-app-pub-3940256099942544/2247696110";
+    public static String Banner_Admob="ca-app-pub-3940256099942544/6300978111";
+    public static String Reward_Admob="ca-app-pub-3940256099942544/5224354917";
+    //----------Yndex Ad----------------
+    public static String Interstitial_Yandex=" ";
+    public static String Native_Yandex=" ";
+    public static String Banner_Yandex=" ";
+    public static String Reward_Yandex=" ";
+    //----------Applovin Ad----------------
+    public static String NATIVE_APPLOVIN="c1c95e66b4dc9218";
+    public static String BANNER_APPLOVIN="8c98e01245aab703";
+    public static String Applovin_REWARDE="6a4f4c1e9d72f9aa";
+    public static String INTERSTITIAL_APPLOVIN="b3852d4ac106801f";
+    //--------------jSon link for online mood--------
+    public static String JsonLink=" ";
+    //--------------Unity Id-----------------------
+    public static String Unity_Id="";
+    public static boolean testmode=false; //for unity test mode
+    //--------------Appodeal id-------------
+    public static String Appodeal_id="a42fb99a278023d6baaa9b3f5c88bbfa2681e1e538e81d96";
+    //--------IronSource-------------
+    public static String IRONSOURCE_ID="14fd6b8f9";
+    //-------------Active Ads -----------
+    public static String inter_type="applovin";
+    public static String banner_type="applovin";
+    public static String native_type="applovin";
+    public static String rewarde_type="applovin";
+    //-------------CPA -----------
+    public static String CpaUrl = "https://lottiefiles.com/74375-get-started/";
+    public static Boolean ActiveCpa=false;
+    public static String CpaImg="https://st.depositphotos.com/2228340/3119/i/600/depositphotos_31192161-stock-photo-cpa.jpg";
+
+
+
+}
